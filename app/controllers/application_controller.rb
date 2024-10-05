ac7adaa33760955d23baa5d8ae79185506e7ddc6
EC2 Default User <ec2-user@ip-172-31-45-244.ap-northeast-1.compute.internal>
2024-10-05 09:28:21 +0000

    save

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id) #userのshowページ
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email]) #sign_upと[:email]を引数に渡しているのでサインアップ時にemailを許可
+  end
+
+end
